2ed0992d086f0e0689ad642271f7f20f33ddf82a
Zamir Narvaez <zamiralexisnarvaez@gmail.com>
2022-02-20 19:14:34 -0500

    Fixed naming

--- src/FilesManager.Application/Common/Interfaces/ITagService.cs
+++ src/FilesManager.Application/Common/Interfaces/ITagService.cs
@@ -8,7 +8,7 @@ namespace FilesManager.Application.Common.Interfaces
     public interface ITagService
     {
         Task<IEnumerable<Tag>> CreateCollection(IEnumerable<Tag> filesMetadata);
-        Task RemoveCollection(IEnumerable<Tag> ids);
+        Task RemoveCollection(IEnumerable<Tag> tags);
         Task RemoveAssignments(FileMetadata fileMetadata);
         Task<FileSetTagsModel> AssignTags(FileMetadata file, IEnumerable<Tag> tags);
         Task<IEnumerable<Tag>> SearchByValue(IEnumerable<string> tags);
